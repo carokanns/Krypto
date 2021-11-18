69a9995aad43fb7b213c0d0ecd63d6294713fe1d
carokanns <jp@fendrich.se>
2021-11-18 12:50:11 +0100

    kan kra men något är fel

--- Binance_krypto/Binance_buy_sell.py
+++ Binance_krypto/Binance_buy_sell.py
@@ -1,4 +1,6 @@
-# Buy and sell coins based on Binance_live_crypto.ipynb
+# %% [markdown]
+# # Buy and sell coins based on Binance_live_crypto.ipynb
+# ### Thanks to Algovibes YouTube channel
 
 # %%
 import asyncio
@@ -32,64 +34,109 @@ for symbol in symbols:
 
 
 # %%
-top_coin = symbols[rets.index(max(rets))]
-print(top_coin, max(rets))
+if len(rets)>0:
+    top_coin = symbols[rets.index(max(rets))]
+    print(top_coin, max(rets))
+else:
+    top_coin=None
+    print('No coins to buy')
 
 
 # %%
-investment_amount = 300
+investment_amount = float(300)
+MY_BALANCE = float(500)  ##### This is a temporary variable before we go sharp
+ 
+def get_my_balance():
+    global MY_BALANCE
+    if False:
+        free_usd = [i for i in client.get_account()['balances'] if i['asset']=='USDT'][0]['free']
+    
+    # temporary override free_usd
+    free_usd =  MY_BALANCE
+    
+    return free_usd
+
+
+# %%
+def create_buy_order(symbol, quantity,price):
+    global MY_BALANCE
+    if False: 
+        order = client.order_market_buy(symbol=symbol, side='BUY',type='MARKET', quantity=quantity)
+    
+    # temporary until we go sharp
+    priset=quantity*price
+    order = {'fills':[{'price':price},{'price':price} ]}
+    MY_BALANCE += priset
+    return order
+
+
+# %%
+
 info=client.get_symbol_info(symbol=top_coin)
 LotSize = float([i for i in info['filters'] if i['filterType']=='LOT_SIZE'][0]['stepSize'])
-prize = float(client.get_symbol_ticker(symbol=top_coin)['price'])
+price = float(client.get_symbol_ticker(symbol=top_coin)['price'])
 decimals = len(str(LotSize).split('.')[1])
-buy_quantity = round(investment_amount/prize/LotSize,decimals)
+buy_quantity = round(investment_amount/price,decimals)
 
-free_usd = [i for i in client.get_account()['balances'] if i['asset']=='USDT'][0]['free']
-if float(free_usd) < investment_amount:
-    order = client.order_market_buy(symbol=top_coin, side='BUY',type='MARKET', quantity=buy_quantity)
+free_usd = get_my_balance()
+if float(free_usd) > investment_amount:
+    order=create_buy_order(top_coin,buy_quantity,price)
     buyprice = float(order['fills'][0]['price'])
-    print(top_coin,buy_quantity,buyprice)
+    print(f'Köpte {buy_quantity} {top_coin} för {buyprice}. Totalt: {buyprice*buy_quantity}')
 else:
-    print('You have enough USDT to buy the coin')
+    buyprice=None
+    print('You have not enough USDT to buy the coin', top_coin, free_usd, investment_amount)
     quit()
 
 
 # %%
-investment_amount/prize/LotSize, buy_quantity*prize
-
-
-# %%
 def createframe(msg):
-    df = pd.DataFrame([msg['data']])
-    df['symbol'] = msg['stream']
+    df = pd.DataFrame([msg])
     df = df.loc[:,['s','E','p']]
     df.columns=['symbol', 'Time', 'Price']
     df.Price = df.Price.astype(float)
     df.Time = pd.to_datetime(df.Time, unit='ms')
     return df
 
-def create_sell_order(coin, quantity):
-    order = client.create_order(symbol=coin, side='SELL', type='MARKET', quantity=quantity)
+def create_sell_order(symbol, quantity,price):
+    global MY_BALANCE
+    if False:
+        order = client.create_order(symbol=symbol, side='SELL', type='MARKET', quantity=quantity)
+    
+    # temporary until we go sharp
+    priset=price*quantity
+    order = {'fills':[{'price':price},{'price':price} ]}
+    MY_BALANCE -= priset
     return order
 
 
 # %%
 async def main(coin):
     # client = await AsyncClient.create()
+    # print('start main')
+    # set start time
+    start_time = dt.datetime.now()
     bm = BinanceSocketManager(client)
     ts = bm.trade_socket(coin)
     async with ts as tscm:
+        # print('start trade loop')
         while True:
             res = await tscm.recv()
             if res:
+                # set elapsed time since start
+                elapsed_time = dt.datetime.now() - start_time
                 frame = createframe(res)
-                if frame.Price[0] < buyprice*0.97 or frame.Price[0] > buyprice*1.05:
-                    order = create_sell_order(coin, buy_quantity)
-                    buyprice = float(order['fills'][0]['price'])
-                    print(top_coin,buy_quantity, buyprice )
-                    loop.stop()
+                if elapsed_time.seconds > 300: # 5 minutes
+                    start_time = dt.datetime.now()
+                    print(frame.Price[0],'min:',round(buyprice*0.97,3), 'max:', round(buyprice*1.03,3))
+                if frame.Price[0] < buyprice*0.97 or frame.Price[0] > buyprice*1.03:
+                    order = create_sell_order(coin, buy_quantity,frame.Price[0])
+                    print(f'Sålde {buy_quantity} {top_coin} för {frame.Price[0]}. Totalt: {buy_quantity*frame.Price[0]}' )
+                    print('\n',order)
+                    # loop.stop()
+                    break
     
-    await client.close_connection()
+    # await client.close_connection()
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(top_coin))
