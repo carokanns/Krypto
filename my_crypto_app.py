d0f6fb362f0459773e36d2760de131635feb848c
carokanns <jp@fendrich.se>
2022-01-25 14:19:25 +0100

    Nu med Ichimoku

--- my_crypto_app.py
+++ my_crypto_app.py
@@ -1,5 +1,9 @@
 # Använd 'krypto_skapa_modell för att uppdater modellerna till senaste datum
 
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.express as px
+# import cufflinks as cf
 import numpy as np
 import pandas as pd
 # from datetime import datetime as dt
@@ -10,10 +14,18 @@ import yfinance as yf
 from matplotlib import pyplot as plt
 
 import streamlit as st
-# from IPython.display import clear_output 
-from catboost import CatBoostRegressor,Pool,utils
+# from IPython.display import clear_output
+from catboost import CatBoostRegressor, Pool, utils
 # import time
 plt.style.use('fivethirtyeight')
+
+
+st.set_page_config(
+    page_title="JP Crypto Currencies",
+    page_icon="🧊",
+    # layout="wide",
+    initial_sidebar_state="auto"
+)
 
 def _max_width_():
     max_width_str = f"max-width: 2000px;"
@@ -45,7 +57,7 @@ def vol_ret_kolumner(df):
         proc_change(df,'vol','Volume',i+1)
     return df  
     
-def define_new_columns(df):
+def define_prognos_columns(df):
     df = vol_ret_kolumner(df)
     
     df['ret_y'] = df.ret_1.copy()
@@ -75,7 +87,7 @@ def add_row(df):
     new_data['day']=  None # pd.DatetimeIndex(new_date).day
     df = df.append(new_data)
     
-    df = define_new_columns(df)
+    df = define_prognos_columns(df)
     return df
 
 # för predict och graf
@@ -142,6 +154,106 @@ def predict_n_days(df,kryptotext, relativ=False):
     return df    
 
 
+def add_bollinger(df):
+    df['SMA'] = df['Adj Close'].rolling(window=20).mean()
+    df['stddev'] = df['Adj Close'].rolling(window=20).std()
+    df['Upper'] = df.SMA + 2*df.stddev
+    df['Lower'] = df.SMA - 2*df.stddev
+
+    df['Buy_Signal'] = np.where(
+        df.Lower > df['Adj Close'], True, False)
+    df['Sell_Signal'] = np.where(
+        df.Upper < df['Adj Close'], True, False)
+    return df
+
+
+def add_Ichimoku_to_df(df_):
+    df = df_.copy()
+    # Conversion
+    hi_val = df['High'].rolling(window=9).max()
+    low_val = df['Low'].rolling(window=9).min()
+    df['Conversion'] = (hi_val + low_val) / 2
+
+    # Baseline
+    hi_val2 = df['High'].rolling(window=26).max()
+    low_val2 = df['Low'].rolling(window=26).min()
+    df['Baseline'] = (hi_val2 + low_val2) / 2
+
+    # Span A
+    df['SpanA'] = ((df['Conversion'] + df['Baseline']) / 2).shift(26)
+
+    # Span B
+    hi_val3 = df['High'].rolling(window=52).max()
+    low_val3 = df['Low'].rolling(window=52).min()
+    df['SpanB'] = ((hi_val3 + low_val3) / 2).shift(26)
+
+    # Lagging Span
+    df['Lagging'] = df['Close'].shift(-26)
+
+    return df
+
+
+def get_fill_color(label):
+    if label >= 1:
+        return 'rgba(0,250,0,0.4)'
+    else:
+        return 'rgba(250,0,0,0.4)'
+def plot_ichimoku(df, title):
+    # set candlestick green and red
+    candle = go.Candlestick(x=df.index, open=df['Open'],
+                            high=df['High'], low=df["Low"], close=df['Close'], name="Candlestick") 
+    
+    df1 = df.copy()
+    fig = go.Figure()
+    df['label'] = np.where(df['SpanA'] > df['SpanB'], 1, 0)
+    df['group'] = df['label'].ne(df['label'].shift()).cumsum()
+
+    df = df.groupby('group')
+
+    dfs = []
+    for name, data in df:
+        dfs.append(data)
+
+    for df in dfs:
+        fig.add_traces(go.Scatter(x=df.index, y=df.SpanA,
+                                  line=dict(color='rgba(0,0,0,0)')))
+
+        fig.add_traces(go.Scatter(x=df.index, y=df.SpanB,
+                                  line=dict(color='rgba(0,0,0,0)'),
+                                  fill='tonexty',
+                                  fillcolor=get_fill_color(df['label'].iloc[0])))
+
+    baseline = go.Scatter(x=df1.index, y=df1['Baseline'],
+                          line=dict(color='orange', width=2), name="Baseline")
+
+    conversion = go.Scatter(x=df1.index, y=df1['Conversion'],
+                            line=dict(color='black', width=1), name="Conversion")
+
+    lagging = go.Scatter(x=df1.index, y=df1['Lagging'],
+                         line=dict(color='purple', width=2), name="Lagging")
+
+    span_a = go.Scatter(x=df1.index, y=df1['SpanA'],
+                        line=dict(color='green', width=2, dash='dot'), name="Span A")
+
+    span_b = go.Scatter(x=df1.index, y=df1['SpanB'],
+                        line=dict(color='red', width=1, dash='dot'), name="Span B")
+
+    fig.add_trace(candle)
+    fig.add_trace(baseline)
+    fig.add_trace(conversion)
+    fig.add_trace(lagging)
+    fig.add_trace(span_a)
+    fig.add_trace(span_b)
+
+    # set title
+    my_title = f'{title} - (<i>zoom in/out i grafen under</i>)'
+    
+    fig.update_layout(title=my_title)
+    
+    fig.update_layout(height=800, width=1300, showlegend=True , paper_bgcolor='rgba(255,255,255,0)', plot_bgcolor='rgba(255,255,255,0.8)')
+
+    st.write(fig)
+
 load = st.container()
 graf = st.container()
 
@@ -153,15 +265,18 @@ alternativ_text = """
 global valuta
 valuta='ETH-USD'
 typ=1
-typtxt = st.sidebar.selectbox('Vad göra?',('Köra prognos per kryptovaluta','5-dagars relativ prognos för alla krypto','Jämföra krypto med Sthlm-börsen'),index=0)
+typtxt = st.sidebar.selectbox('Vad göra?',('Graf per kryptovaluta','5-dagars relativ prognos för alla krypto','Jämföra krypto med Sthlm-börsen'),index=0)
 if typtxt[:3] == 'Jäm':             # jämför med OMX
     typ=1
 elif  typtxt[:3] == '5-d':          # 5-dagars prognos för alla
     typ=3
-else:                               # detaljerad prognos per valuta
+else:                               # detaljerad graf (ev prognos) per valuta
     typ=2
     
-if typ==2:    # prognos per valuta
+if typ==2:    # graf per valuta
+    # graftyp = 'Enkel graf med prognos'
+    graftyp = st.sidebar.selectbox('välj graf-typ', ('Enkel graf med prognos', 'Bollinger-graf med prognos', 'Ichimoku-graf'))
+
     kryptotext = st.sidebar.selectbox('vilken valuta',('ETH (Ethereum)','BTC (Bitcoin)',
                                                     'BCH (Bitcoin Cach)','ZRX (0x)','XRP'),index=0)
     kryptotext = kryptotext[:3]
@@ -175,8 +290,7 @@ if typ==2:    # prognos per valuta
         valuta = 'ZRX-USD'   
     elif kryptotext=='XRP':
         valuta = 'XRP-USD'  
-        
-        
+            
     with load:
         # valuta = web.DataReader(valuta,'yahoo') # Etherium
         valuta = yf.download(valuta,progress=False)
@@ -184,119 +298,137 @@ if typ==2:    # prognos per valuta
         # st.write('de 5 sista dagarna exrtrakt',valuta.iloc[-5:][['Adj Close', 'Volume',]])
             
     # st.write('lastval',valuta.iloc[-1:].index[0])
-    tidsram='30 dagar'
-    tidsram=st.sidebar.selectbox('tidsram för graf',('15 dagar','30 dagar','90 dagar','från inköp'),1)
-    bollinger='Ja'
-    bollinger=st.sidebar.selectbox('Bollinger-graf',('Ja','Nej'))
-    if tidsram=='15 dagar':
-        tidsram=15
-    elif tidsram=='30 dagar':
-        tidsram=30
-    elif tidsram=='90 dagar':
-        tidsram=90
-    elif tidsram=='från inköp':
-        startdat = '2021-04-12'
-        partlen=len(valuta.loc[:startdat])-1
-        totlen=len(valuta)
-        tidsram = totlen-partlen
-    else:
-        pass
-
-    alternativ = st.sidebar.selectbox('Alternativ prognos',('Ja','Nej'),index=1)
-
-    with st.sidebar.expander('Förklaring av alternativ prognos'):
-        st.info(alternativ_text)
-        
-    def add_bollinger(df):    
-        df['SMA'] = df['Adj Close'].rolling(window=20).mean()
-        df['stddev'] = df['Adj Close'].rolling(window=20).std()
-        df['Upper'] = df.SMA + 2*df.stddev
-        df['Lower'] = df.SMA - 2*df.stddev
-
-        df['Buy_Signal'] = np.where(df.Lower > df['Adj Close'],True,False)
-        df['Sell_Signal'] = np.where(df.Upper < df['Adj Close'],True,False)
-        return df
-               
-    
+      
+    tidsram = '30 dagar'   
     with graf:  
-        
-        data = define_new_columns(valuta.copy())  # remove unused columnes and add new columns
-        
-        if alternativ=='Nej':
-            data = predict_n_days(data.copy(),kryptotext)
-        else:    
-            data = predict_alt_n_days(data.copy(),kryptotext)
-        
-        if bollinger=='Ja':
-            data=add_bollinger(data)
-        else:
-            pass
-            
-        # st.write('tidsram =',tidsram,'dagar för grafen')
-        lastdate=valuta.iloc[-1:].index
-        st.write('Senast kända datum', str(lastdate[0])[:10]+'. (Efter den röda prickade linjen följer en 5 dagars prognos)')
-        
-        ### plot Adj Close ###
-        fig = plt.figure(figsize=(16,6))
-        ax = fig.add_subplot(1,1,1)
-        df=data.iloc[-tidsram:]
-        if alternativ=='Ja':
-            ax.set_title(kryptotext+' "Adjusted Close" Alternativ prognos')
-        else:
-            ax.set_title(kryptotext+' "Adjusted Close"')            
-        
-        if bollinger=='Nej':
-            ax.plot(
-                df.index,
-                df["Adj Close"],color='b',
-            )   
-            maxa = df['Adj Close'].max()
-            mina = df['Adj Close'].min()
-            
-            ax.plot(
-                df.index,
-                df[["Adj Close"]],
-            )    
-        else:
-            maxa = df['Upper'].max()
-            mina = df['Lower'].min()
-        
-            ax.plot(
-                df.index,
-                df[["Adj Close",'SMA','Upper','Lower']]
-            )    
-            ax.fill_between(df.index,df.Upper,df.Lower,color='grey',alpha=0.3)
-            ax.legend(['Pris (Adj Close)','Simple Moving Avg','Övre','Undre'])
+        if graftyp[:8] != 'Ichimoku':
+            tidsram = st.sidebar.selectbox('tidsram för graf', ('15 dagar', '30 dagar', '90 dagar', 'från inköp'), 1)
+            if tidsram == '15 dagar':
+                tidsram = 15
+            elif tidsram == '30 dagar':
+                tidsram = 30
+            elif tidsram == '90 dagar':
+                tidsram = 90
+            elif tidsram == 'från inköp':
+                startdat = '2021-04-12'
+                partlen = len(valuta.loc[:startdat])-1
+                totlen = len(valuta)
+                tidsram = totlen-partlen
+            else:
+                pass
 
-            # ax.set_xlabel("Datum")
-        ax.set_ylabel("$USD")
-        ax.tick_params(axis='x', rotation=66)
-            
-        ax.vlines(lastdate,mina,maxa,colors='r', linestyles='dotted')
-        st.write(fig)
-            
-        fig2 = plt.figure(figsize=(16,6))
-        
-        ax2 = fig2.add_subplot(1,1,1)
+            data = define_prognos_columns(valuta.copy())
 
-        ax2.set_title(kryptotext+' Volymer')
-        
-        ax2.set_ylabel("Miljoner")
-        ax2.tick_params(axis='x',rotation=66)
-        ax2.plot(
-            df.index,
-            df['Volume']/1000000,color='g'
-        )
-        maxv = df['Volume'].max()/1000000
-        minv = df['Volume'].min()/1000000
-        ax2.vlines(lastdate,minv,maxv,colors='r', linestyles='dotted')
-        
-        st.write(fig2)
-        
+            alternativ = st.sidebar.selectbox(
+                'Alternativ prognos', ('Ja', 'Nej'), index=1)
+            if alternativ == 'Nej':
+                data = predict_n_days(data.copy(), kryptotext)
+            else:
+                data = predict_alt_n_days(data.copy(), kryptotext)
+
+            with st.sidebar.expander('Förklaring av alternativ prognos'):
+                st.info(alternativ_text)
+
+            # st.write('tidsram =',tidsram,'dagar för grafen')
+            lastdate=valuta.iloc[-1:].index
+            st.write('Senast kända datum', str(lastdate[0])[:10]+'. (Efter den röda prickade linjen följer en 5 dagars prognos)')
+            
+            ### plot Adj Close ###
+            fig = plt.figure(figsize=(16,6))
+            ax = fig.add_subplot(1,1,1)
+            
+            if alternativ=='Ja':
+                ax.set_title(kryptotext+' "Adjusted Close" Alternativ prognos')
+            else:
+                ax.set_title(kryptotext+' "Adjusted Close"')   
+    
+            if graftyp[:4]=='Boll':
+                df = add_bollinger(data)
+                # ax.plot(
+                #     df.index,
+                #     df["Adj Close"], color='b',
+                # )
+                df = df.iloc[-tidsram:]
+                maxa = df['Upper'].max()
+                mina = df['Lower'].min()
+
+                ax.plot(
+                    df.index,
+                    df[["Adj Close",'SMA','Upper','Lower']]
+                )    
+                ax.fill_between(df.index,df.Upper,df.Lower,color='grey',alpha=0.3)
+                ax.legend(['Pris (Adj Close)','Simple Moving Avg','Övre','Undre'])
+
+                # ax.set_xlabel("Datum")
+                ax.set_ylabel("$USD")
+                ax.tick_params(axis='x', rotation=66)
+                    
+                ax.vlines(lastdate,mina,maxa,colors='r', linestyles='dotted')
+                st.write(fig)
+                    
+                fig2 = plt.figure(figsize=(18, 4))
+
+                ax2 = fig2.add_subplot(1,1,1)
+
+                ax2.set_title(kryptotext+' Volymer')
+                
+                ax2.set_ylabel("Miljoner")
+                ax2.tick_params(axis='x',rotation=66)
+                ax2.plot(
+                    df.index,
+                    df['Volume']/1000000,color='g'
+                )
+                maxv = df['Volume'].max()/1000000
+                minv = df['Volume'].min()/1000000
+                ax2.vlines(lastdate,minv,maxv,colors='r', linestyles='dotted')
+                
+                st.write(fig2)
+            else:
+                # Enkel graf med prognos
+                df = data.iloc[-tidsram:]
+                maxa = df['Adj Close'].max()
+                mina = df['Adj Close'].min()
+
+                ax.set_ylabel("$USD")
+                ax.tick_params(axis='x', rotation=66)
+                ax.vlines(lastdate, mina, maxa,
+                          colors='r', linestyles='dotted')
+                ax.plot(
+                    df.index,
+                    df[["Adj Close"]]
+                )
+                st.write(fig)
+                fig2 = plt.figure(figsize=(18, 4))
+
+                ax2 = fig2.add_subplot(1, 1, 1)
+
+                ax2.set_title(kryptotext+' Volymer')
+
+                ax2.set_ylabel("Miljoner")
+                ax2.tick_params(axis='x', rotation=66)
+                ax2.plot(
+                    df.index,
+                    df['Volume']/1000000, color='g'
+                )
+                maxv = df['Volume'].max()/1000000
+                minv = df['Volume'].min()/1000000
+                ax2.vlines(lastdate, minv, maxv,
+                           colors='r', linestyles='dotted')
+
+                st.write(fig2)
+        else: # Ichimoku
+            startdat = '2021-04-12'
+            partlen = len(valuta.loc[:startdat])-1
+            totlen = len(valuta)
+            totram = totlen-partlen
+            dfi = valuta.iloc[-totram:]
+            # background white
+            dfi = add_Ichimoku_to_df(dfi)
+            plot_ichimoku(dfi,f'{kryptotext} "Adjusted Close"')
+            
         if st.button('inspektera '+kryptotext+' data'):
             st.write('tidsram', tidsram)
-            df=data.iloc[-tidsram:]
-            st.write(df)
+            st.dataframe(data.iloc[-tidsram:])
         else:
             pass    
         
@@ -343,7 +475,7 @@ else: # typ==3 prognos för alla
     alternativ = st.sidebar.selectbox('Alternativ prognos',('Ja','Nej'),index=1)
     with st.sidebar.expander('Förklaring av alternativ prognos'):
         st.info(alternativ_text)
-
+    df=pd.DataFrame()
     with st.spinner('ta det lugnt'):
         # df = relret(web.DataReader(tickers,'yahoo',start)['Adj Close']) # alla mina krypto
         start='2021-08-01'
@@ -358,11 +490,12 @@ else: # typ==3 prognos för alla
         for ticker in tickers:   
             the_text = ticker[:3]
             data = allt[[('Adj Close',ticker),('Volume',ticker),('Close',ticker)]].copy()
-            
+            data = data.dropna()
             data.columns = ['Adj Close', 'Volume','Close']
-            data = define_new_columns(data)  # remove unused columnes and add new columns
+            data = define_prognos_columns(data)  # remove unused columnes and add new columns
             cols = list(data.columns)
             if alternativ=='Nej':
+                # st.write(data)
                 data = predict_n_days(data.copy(),the_text)
             else:
                 data = predict_alt_n_days(data.copy(),the_text)
