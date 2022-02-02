3e4b274329d9886a8ed19acb7456ce7967234b68
carokanns <jp@fendrich.se>
2022-02-02 14:29:53 +0100

    removed dropna from 'Enkel graf'

--- my_crypto_app.py
+++ my_crypto_app.py
@@ -391,7 +391,7 @@ if typ==2:    # graf per valuta
                 st.write(fig2)
             else:
                 # Enkel graf med prognos
-                df = data.iloc[-tidsram:].dropna()
+                df = data.iloc[-tidsram:]
                 maxa = df['Adj Close'].max()
                 mina = df['Adj Close'].min()
 
